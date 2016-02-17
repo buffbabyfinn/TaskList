e60b6091331a1756ca2ec6fa884e695b315fd864
Megan <mefayer@gmail.com>
2016-02-17 13:39:15 -0800

    finish testing and clear all errors

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,6 @@
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
@@ -17,6 +18,9 @@ public class AppTest extends FluentTest {
 
   @ClassRule
   public static ServerRule server = new ServerRule();
+
+  @Rule
+  public ClearRule ClearRule = new ClearRule();
 
 
   @Test
